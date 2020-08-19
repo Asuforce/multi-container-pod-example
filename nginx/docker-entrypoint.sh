4f09820e8f36bf0a8613a6d944143da6ddee555b
Manabu Matsuzaki <matsumana@gmail.com>
2020-08-20 02:21:59 +0900

    Polish

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -5,4 +5,4 @@ nginx
 # Since nginx-prometheus-exporter can't start if can't scrape metrics from nginx, wait for a while.
 sleep 1
 
-/nginx-prometheus-exporter/nginx-prometheus-exporter -nginx.scrape-uri http://127.0.0.1:18081/stub_status
+exec /nginx-prometheus-exporter/nginx-prometheus-exporter -nginx.scrape-uri http://127.0.0.1:18080/stub_status
