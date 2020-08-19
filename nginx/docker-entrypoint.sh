6705d658c6b03ada082ae1d88958538d08fc46cc
Manabu Matsuzaki <matsumana@users.noreply.github.com>
2020-08-20 03:35:42 +0900

    Add comment

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+# Start in the background
 nginx
 
 # Since nginx-prometheus-exporter can't start if can't scrape metrics from nginx, wait for a while.
 sleep 1
 
+# Need to use `exec`. otherwise shutdown signal can't reach to nginx-prometheus-exporter
 exec /nginx-prometheus-exporter/nginx-prometheus-exporter -nginx.scrape-uri http://127.0.0.1:18080/stub_status
